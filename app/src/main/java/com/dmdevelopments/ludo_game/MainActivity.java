c8549b0a81e7c2ffcde6480be1965c092b5e4db1
androidtramo <android@tramo.in>
2025-01-23 13:01:27 +0530

    ok

---
+++ app/src/main/java/com/dmdevelopments/ludo_game/MainActivity.java
@@ -0,0 +1,23 @@
+package com.dmdevelopments.ludo_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onPlayClick(View v)
+    {
+        startActivity(new Intent(getApplicationContext(),LudoActivity.class));
+    }
+}
